5321facb776fed86dfc8e6adf9c0f2a016a828f8
ramosreenata <78076740+ramosreenata@users.noreply.github.com>
2023-10-06 10:42:06 +1100

    Update API_NER.py

--- API_NER.py
+++ API_NER.py
@@ -8,21 +8,33 @@ Created on Fri Oct  6 09:05:08 2023
 from flask import Flask, request, jsonify
 import spacy
 
+
 app = Flask(__name__)
 
-nlp = spacy.load('es_core_news_sm')
+# Descargamos el modelo en español
+nlp = spacy.load('es_core_news_sm') 
 
+# Especificamos que es un POST request y añadimos /ner al URL
 @app.route('/ner', methods=['POST'])
+
 def recognize_entities():
     try:
+        # Hacemos el request de la data
         data = request.json
-        texto = data['texto']
-        doc = nlp(texto)
-        entidades = [(ent.text, ent.label_) for ent in doc.ents]
-        return jsonify({"entidades": entidades})
+        oraciones = data['oraciones']
+        results = []
+
+        for oracion in oraciones:
+            doc = nlp(oracion)
+            # Extraemos las entidades
+            entities = [(ent.text, ent.label_) for ent in doc.ents]
+            # Juntamos la oración con el resultado
+            results.append({"oración": oracion, "entidades": entities})
+
+        return jsonify({"resultado": results})
+    
     except Exception as e:
         return jsonify({"error": str(e)}), 400
-
 
 if __name__ == '__main__':
     app.run(debug=True)
